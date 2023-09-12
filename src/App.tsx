460df6ab6554b43063f71498d906b1d7bb512dde
Felipe Kafuri <felipe11.rk@gmail.com>
2023-09-12 08:42:04 -0300

    install shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+import { Button } from './components/ui/button'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-
-    <h1>Hello world!</h1>
-
+      <>
+      <Button onClick={() => setCount(count + 1)}>test</Button></>
     )
 }
 
